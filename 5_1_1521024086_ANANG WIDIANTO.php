138a76f074bb31afb7b3942d292b0fa70ef00ff8
AnangWidianto <anangw807@gmail.com>
2016-10-12 19:56:01 +0700

    Study Kasus 05
    Dalam study kasus ini ada 4 soal
    1.SK05_1
    2.SK05_2
    3.SK05_3
    4.SK05_4

---
+++ 5_1_1521024086_ANANG WIDIANTO.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+     <title> AKN Bojonegoro </title>
+</head>
+<body>
+      <?php  
+           $kd = array
+                (
+                array("1001","sabun lifeboy","1500"),
+                array("1002","permen blester","5600"),
+                array("1003","pasta gigi pepsodent","4560"),
+                array("1004","madu arbain","30000"),
+                array("1005","kecap ABC","7250"),
+                array("1006","tomat ABC","6700"),
+                array("1007","gula gulaku","89"),
+                array("1008","rinso","7100"),
+                array("1009","super pel","6450"),
+                array("10010","permen tanggo","5600"),
+                );
+      ?>
+</body>
+</html>
